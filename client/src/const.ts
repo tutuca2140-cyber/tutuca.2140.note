a65deb261f519c0fbe0b81a8e0b8ab1e1fb14ca5
tutuca2140-cyber <tutuca.2140@gmail.com>
2026-08-24 16:49:31 -0300

    Update const.ts

--- client/src/const.ts
+++ client/src/const.ts
@@ -3,4 +3,3 @@ export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
 // O Note Note utiliza somente autenticação local.
 // Usuários são criados e administrados pelo Super Administrador.
 export const getLoginUrl = () => "/login";
-};
