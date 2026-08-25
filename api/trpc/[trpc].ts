e48ad3e670349a5100e0a5e0ba065a81dbe8882a
tutuca2140-cyber <tutuca.2140@gmail.com>
2026-08-24 22:09:35 -0300

    fix: bundle tRPC server modules on Vercel

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../../server/routers.ts";
-import { createContext } from "../../server/_core/context.ts";
+import { appRouter } from "../../server/routers";
+import { createContext } from "../../server/_core/context";
 
 const app = express();
 
