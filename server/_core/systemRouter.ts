22f816b77c3163a1a479ecd8aa7c9855f62c434a
tutuca2140-cyber <tutuca.2140@gmail.com>
2026-08-25 10:51:50 -0300

    chore: remove preview write probe

--- server/_core/systemRouter.ts
+++ server/_core/systemRouter.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 import { notifyOwner } from "./notification";
 import { adminProcedure, publicProcedure, router } from "./trpc";
-import { TRPCError } from "@trpc/server";
-import * as db from "../db";
 
 export const systemRouter = router({
   health: publicProcedure
@@ -14,15 +12,6 @@ export const systemRouter = router({
     .query(() => ({
       ok: true,
     })),
-
-  previewWriteCheck: publicProcedure
-    .input(z.object({ key: z.string().min(1) }))
-    .mutation(async ({ input }) => {
-      if (process.env.VERCEL_ENV !== "preview" || input.key !== process.env.VERCEL_GIT_COMMIT_SHA) {
-        throw new TRPCError({ code: "NOT_FOUND", message: "Not found" });
-      }
-      return db.runPreviewWriteCheck();
-    }),
 
   notifyOwner: adminProcedure
     .input(
